5c0a8237f1ea9c533c27505ca12517eb55ef8b62
kdh <kdh462601@gmail.com>
2024-12-11 10:26:12 +0900

    불필요한 코드 삭제

--- JAVA/2024_12_10/src/main/java/day241210_arr/Array_chop.java
+++ JAVA/2024_12_10/src/main/java/day241210_arr/Array_chop.java
@@ -6,17 +6,16 @@ import java.util.Arrays;
 
 public class Array_chop {
     public static void main(String[] args) {
-        int[] numbers = {4,5,6};
-        int start = 0;
-        int end = 2;
+        int[] numbers = {1,2,3,4,5};
+        int start = 2;
+        int end = 3;
         int[] answer = solution(numbers,start,end);
 
         System.out.println(Arrays.toString(answer));
     }
 
     public static int[] solution(int[] numbers, int start, int end) {
-        int[] answer = new int[end];
-        answer = Arrays.copyOfRange(numbers,start,end+1);
+        int[] answer = Arrays.copyOfRange(numbers,start,end+1);
 
         return answer;
 

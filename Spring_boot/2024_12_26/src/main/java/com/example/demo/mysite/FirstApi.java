6460da6c5ff04ee0f7a70f6f2b0ab74878a109ab
kdh <kdh462601@gmail.com>
2024-12-26 15:42:58 +0900

    Spring 소개

---
+++ Spring_boot/2024_12_26/src/main/java/com/example/demo/mysite/FirstApi.java
@@ -0,0 +1,18 @@
+package com.example.demo.mysite;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstApi {
+
+    @GetMapping("/")
+    public String magePage(){
+        return "hello spring";
+    }
+
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello User";
+    }
+}
